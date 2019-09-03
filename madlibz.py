3bf1104358c0760f1225b350eb3a0dddff3a80cd
Arvin Seifipour <arvinseifipour@Arvins-MacBook-Pro.local>
2019-09-02 21:21:34 -0700

    added title

---
+++ madlibz.py
@@ -0,0 +1,11 @@
+#PYTHON3 MADLIBS, story used from https://www.woojr.com/travel-mad-libs-for-kids/were-going-on-an-airplane/
+print("We are going on an airplane madlibs")
+
+This will be my first time {} on a {} and I am so  {}.
+First, we will {} our suitcases with things we will {}
+on vacation like {}, {}, and {} {}.
+Next, we will {} to the airport and {} our car in the {} lot. The
+{} is very {} so we need to ride a {} from the parking lot to the airport.
+Then we will {} our suitcases, which will {} in the {} of the plane during
+the flight. I am excited to {} what things look like from up
+high in the {}!
